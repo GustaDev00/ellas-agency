23c163ad6d7c458be57bb20a8b39befff3efbb30
Gustavo pontes <gustavo.4dev@gmail.com>
2024-12-02 14:45:46 -0300

    init

--- src/components/atoms/logo/index.tsx
+++ src/components/atoms/logo/index.tsx
@@ -1,9 +1,8 @@
 import * as S from "./styles";
-import { LogoIcon } from "@/components/svgs/logo";
 
 export const Logo = ({ className }: { className?: string }) => (
   <S.Logo className={className}>
-    <S.Name>ella's</S.Name>
+    <S.Name>ella&apos;s</S.Name>
     <S.Small>PR Agency</S.Small>
   </S.Logo>
 );
